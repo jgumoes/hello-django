b8be1c0b1507f0e40d4329cc896399be1bbe8b58
Jack Gumoes <j.gumoes@gmail.com>
2022-09-05 19:59:59 +0100

    refactored index templating function

--- hello_django/polls/views.py
+++ hello_django/polls/views.py
@@ -1,16 +1,13 @@
-from curses.ascii import HT
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from .models import Question
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  template = loader.get_template('polls/index.html')
   context = {
     'latest_question_list': latest_question_list,
   }
-  return HttpResponse(template.render(context, request))
+  return HttpResponse(render(request, 'polls/index.html', context))
 
 # Create your views here.
 
