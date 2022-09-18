813e20aa87c87cb95d35651200f72268acadcda9
Jack Gumoes <j.gumoes@gmail.com>
2022-09-18 14:45:25 +0100

    refactored to use djangos built-in error catching

--- hello_django/polls/views.py
+++ hello_django/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Question
 
@@ -12,10 +12,7 @@ def index(request):
 # Create your views here.
 
 def detail(request, question_id):
-  try:
-    question = Question.objects.get(pk=question_id)
-  except Question.DoesNotExist:
-    raise Http404("Question does not exist")
+  question = get_object_or_404(Question, pk=question_id)
   output = f"You're looking at question {question_id}: {question}"
   return HttpResponse(output)
 
